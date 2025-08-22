0450b1ac258223c6e0376c686465dd5ab2f0a88e
FaFo85 <kleymany@hotmail.com>
2025-08-22 09:23:26 -0400

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCVjYKih-p-stEbPVHJk39a705OU3ug8KQ",
+  authDomain: "prompt-maker-e5314.firebaseapp.com",
+  projectId: "prompt-maker-e5314",
+  storageBucket: "prompt-maker-e5314.firebasestorage.app",
+  messagingSenderId: "1079926312121",
+  appId: "1:1079926312121:web:8fee770ab1ce4080069f68",
+  measurementId: "G-S680B9Q5YV"
+};
+export default firebaseConfig;
